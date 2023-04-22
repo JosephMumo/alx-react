e36db18d0bd9ea7a6f13156cb63661697f487a6f
JosephMumo <josephmumo25@gmail.com>
2023-04-22 14:32:30 +0300

    init

---
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer';
+import Notifications from './Notifications';
+import Login from './Login/Login';
+import { Fragment } from 'react';
+import './utils'
+
+function App() {
+	  return (
+		      <Fragment>
+				<Notifications />
+				<div className="App">
+				<Header />
+		        <hr />
+				<Login />
+				<Footer />
+		      </div>
+			  </Fragment>
+		    );
+}
+
+export default App;
