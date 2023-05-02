f71f5c21cd3693e2907dd096d0a179f1a3ddc700
JosephMumo <josephmumo25@gmail.com>
2023-05-02 18:11:29 +0300

    init

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 
 function CourseListRow({ isHeader=false, textFirstCell, textSecondCell=null }) {
+    const rowStyle = {
+        backgroundColor: '#f5f5f5ab'
+    }
+    const headerStyle = {
+        backgroundColor: '#deb5b545'
+    }
     return (
-        <tr>
+        <tr style={rowStyle}>
             {isHeader ?
                 <>
-                    <th colSpan={textSecondCell == null ? 2 : 1}>{textFirstCell}</th>
+                    <th style={headerStyle} colSpan={textSecondCell == null ? 2 : 1}>{textFirstCell}</th>
                     {textSecondCell !== null &&
-                        <th>{textSecondCell}</th>
+                        <th style={headerStyle} >{textSecondCell}</th>
                     }
                 </> :
                 <>
