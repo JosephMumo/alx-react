050945f8740c0fc6c28c8f78b4b7ec37e2d59881
JosephMumo <josephmumo25@gmail.com>
2023-05-10 19:14:18 +0300

    task 5

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,11 @@
+import { List, Map } from "./node_modules/immutable/dist/immutable";
+
+
+export default function concatElements(page1, page2) {
+    return List(page1).concat.List(page2);
+}
+
+export default function mergeElements(page1, page2) {
+    const mergeObj = Map(page1).merge(Map(page2))
+    return mergeObj;
+}
